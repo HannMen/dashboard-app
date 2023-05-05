118f936d1643eceafbafea95b3d32759b3cbad2c
Sebastian Meza <sebasmezab@gmail.com>
2023-05-05 11:15:34 -0500

    revisión js

--- main.js
+++ main.js
@@ -1,110 +1,113 @@
-// Obtener la referencia a los elementos del DOM
 const selectCountry = document.getElementById("select-country");
+const selectTime = document.getElementById("select-time");
 const myChart = document.getElementById("myChart");
 const confirmedBtn = document.getElementById("confirmed-btn");
 const recoveredBtn = document.getElementById("recovered-btn");
 const deathsBtn = document.getElementById("deaths-btn");
 const defaultOption = document.createElement("option");
+const currentDate = new Date();
+const startDateString = "2020-01-01";
+const endDateString = currentDate.toISOString().slice(0, 10);
+
 defaultOption.text = "Selecciona un país de la lista";
 defaultOption.disabled = true;
 defaultOption.selected = true;
 selectCountry.add(defaultOption);
 
-// Configuración inicial del gráfico
-const chart = new Chart(myChart, {
+const chart = new Chart(document.getElementById("myChart"), {
   type: "line",
   data: {
     labels: [],
     datasets: [
       {
-        label: "Confirmados",
+        label: "",
         data: [],
-        borderColor: "#007bff",
-        fill: false,
-      },
-      {
-        label: "Recuperados",
-        data: [],
-        borderColor: "#6c757d",
-        fill: false,
-      },
-      {
-        label: "Fallecidos",
-        data: [],
-        borderColor: "#dc3545",
+        borderColor: "rgba(75, 192, 192, 1)",
         fill: false,
       },
     ],
   },
   options: {
-    responsive: true,
-    maintainAspectRatio: false,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
   },
 });
 
-// Función para obtener los datos de COVID-19 del país seleccionado y actualizar el gráfico
-const updateChartData = (countrySlug, dataType) => {
+const loadCountries = () => {
+  axios
+    .get("https://api.covid19api.com/countries")
+    .then((response) => {
+      const countries = response.data;
+      countries.forEach((country) => {
+        const option = document.createElement("option");
+        option.value = country.Slug;
+        option.text = country.Country;
+        selectCountry.add(option);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      alert("Error fetching data");
+    });
+};
+
+const updateChartData = (countrySlug, dataType, startDateString, endDateString) => {
   axios
     .get(
-      `https://api.covid19api.com/country/${countrySlug}?from=2020-01-01T00:00:00Z&to=2022-05-01T00:00:00Z`
+      `https://api.covid19api.com/dayone/country/${countrySlug}?from=${startDateString}T00:00:00Z&to=${endDateString}T00:00:00Z`
     )
     .then((response) => {
       const data = response.data;
-      const labels = data.map((d) => d.Date.slice(0, 7));
+      const labels = data.map((d) => d.Date.slice(0, 10));
       const values = data.map((d) => d[dataType]);
       chart.data.labels = labels;
-      switch (dataType) {
-        case "Confirmed":
-          chart.data.datasets[0].data = values;
-          break;
-        case "Recovered":
-          chart.data.datasets[1].data = values;
-          break;
-        case "Deaths":
-          chart.data.datasets[2].data = values;
-          break;
-      }
+      chart.data.datasets[0].label = dataType;
+      chart.data.datasets[0].data = values;
       chart.update();
     })
-    .catch((error) => console.log(error));
+    .catch((error) => {
+      console.log(error);
+      alert("Error fetching data");
+    });
 };
 
-// Obtener la lista de países y agregarlos al menú desplegable
-axios
-  .get("https://api.covid19api.com/countries")
-  .then((response) => {
-    const countries = response.data;
-    countries.sort((a, b) => (a.Country > b.Country ? 1 : -1));
-    countries.forEach((country) => {
-      const option = document.createElement("option");
-      option.value = country.Slug;
-      option.textContent = country.Country;
-      selectCountry.appendChild(option);
-    });
-  })
-  .catch((error) => console.log(error));
+loadCountries();
 
-// Event listener para actualizar el gráfico cuando se selecciona un país
-selectCountry.addEventListener("change", (event) => {
-  const countrySlug = event.target.value;
-  updateChartData(countrySlug, "Confirmed");
-  if (event.target.value !== "") {
-    defaultOption.disabled = true;
-  }
+selectCountry.addEventListener("change", () => {
+  const countrySlug = selectCountry.value;
+  const dataType = confirmedBtn.classList.contains("active")
+    ? "Confirmed"
+    : recoveredBtn.classList.contains("active")
+    ? "Recovered"
+    : "Deaths";
+  updateChartData(countrySlug, dataType, startDateString, endDateString);
 });
 
-// Event listeners para actualizar el gráfico según el botón presionado
 confirmedBtn.addEventListener("click", () => {
+  confirmedBtn.classList.add("active");
+  recoveredBtn.classList.remove("active");
+  deathsBtn.classList.remove("active");
   const countrySlug = selectCountry.value;
-  updateChartData(countrySlug, "Confirmed");
+  updateChartData(countrySlug, "Confirmed", startDateString, endDateString);
 });
 
 recoveredBtn.addEventListener("click", () => {
   const countrySlug = selectCountry.value;
-  updateChartData(countrySlug, "Recovered");
-});
-
-deathsBtn.addEventListener("click", () => {
-  const countrySlug = selectCountry.value;
-  updateChartData(countrySlug, "Deaths");
+  const dataType = "Recovered";
+  updateChartData(countrySlug, dataType, startDateString, endDateString);
   });
+  
+  deathsBtn.addEventListener("click", () => {
+  const countrySlug = selectCountry.value;
+  const dataType = "Deaths";
+  updateChartData(countrySlug, dataType, startDateString, endDateString);
+  });
+  
+  loadCountries(); // load countries when page is loaded initially
